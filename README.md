d35ca902cdaba32224b7e61603a710b3c03c2c75
Cimon Lucas (LCM) <LCimon@voyages-sncf.com>
2016-11-07 13:39:43 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,10 @@
+Useful [pre-commit](http://pre-commit.com) hooks to check your HTML pages / templates.
+
+Check `.pre-commit-config.yaml` in this repo for usage examples and useful local hooks.
+
+## Usage
+
+```
+-   id: forbid-html-img-without-alt-text
+-   id: forbid-non-std-html-attributes
+```
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='pre-commit-hooks-lxml',
+    description='Some HTML validating hooks using lxml for pre-commit',
+    url='https://github.com/Lucas-C/pre-commit-hooks-lxml',
+    version='1.0.0',
+
+    author='Lucas Cimon',
+    author_email='lucas.cimon@gmail.com',
+
+    platforms='linux',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
+    packages=find_packages('.'),
+    install_requires=[
+        'lxml',
+    ],
+    entry_points={
+        'console_scripts': [
+            'forbid_html_img_without_alt_text = pre_commit_hooks.forbid_html_img_without_alt_text:main',
+            'forbid_non_std_html_attributes = pre_commit_hooks.forbid_non_std_html_attributes:main',
+        ],
+    },
+)
